401699f45ef0410d1c6009d13fc51c056b0f8bd1
Facundo Amarilla <facundogabriel91@gmail.com>
2023-05-01 14:33:46 -0300

    correxion de referencias

---
+++ app/frmVentanaPrincipal.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace app
+{
+    partial class frmVentanaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPanel = new System.Windows.Forms.DataGridView();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPanel
+            // 
+            this.dgvPanel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPanel.Location = new System.Drawing.Point(74, 52);
+            this.dgvPanel.Name = "dgvPanel";
+            this.dgvPanel.Size = new System.Drawing.Size(534, 310);
+            this.dgvPanel.TabIndex = 0;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(625, 52);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(163, 23);
+            this.btnActualizar.TabIndex = 1;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // frmVentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.dgvPanel);
+            this.Name = "frmVentanaPrincipal";
+            this.Text = "frnVentanaPrincipal";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPanel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPanel;
+        private System.Windows.Forms.Button btnActualizar;
+    }
+}
---
+++ app/frmVentanaPrincipal.cs
@@ -0,0 +1,29 @@
+﻿using Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace app
+{
+    public partial class frmVentanaPrincipal : Form
+    {
+        NegocioArticulo negocio;
+        public frmVentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        // METODOS
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            negocio = new NegocioArticulo();
+            dgvPanel.DataSource = negocio.Leer();
+        }
+    }
+}
